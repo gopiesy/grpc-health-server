8de0dd10431c2c74c49af4e6cb2b9647ed1f1e27
GopiChand Komma <gkomma@apple.com>
2023-01-06 17:15:51 -0600

    using mTLS

--- client.sh
+++ client.sh
@@ -5,8 +5,19 @@ set -x
 REQ='{"service": "health"}'
 URL='localhost:9111'
 RPC='grpc.health.v1.Health/Check'
+ROOT_CA='./certs/cacert.pem'
+CLIENT_CRT='./certs/client.pem'
+CLIENT_KEY='./certs/client.key'
 
-~/go/bin/grpcurl --plaintext --import-path ./proto --proto health.proto \
+#~/go/bin/grpcurl --plaintext --import-path ./proto --proto health.proto \
+#    -d "$REQ" \
+#    $URL \
+#    $RPC
+
+~/go/bin/grpcurl --insecure --import-path ./proto --proto health.proto \
+    -cacert "$ROOT_CA"\
+    -key "$CLIENT_KEY"\
+    -cert "$CLIENT_CRT"\
     -d "$REQ" \
     $URL \
     $RPC
--- main.go
+++ main.go
@@ -2,46 +2,66 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	server "github.com/gopiesy/grpc-health-server/health-server"
 	health "github.com/gopiesy/grpc-health-server/proto"
-	"github.pie.apple.com/isd-dev/go-applecerts"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
 var (
 	port = flag.Int("port", 9111, "Port on which gRPC health server should listen for TCP conn.")
-	cert = flag.String("cert", "/Users/gopichandkomma/Desktop/certs/previewhealthchecker.edge.apple.com.chain.pem", "server cert")
-	key  = flag.String("key", "/Users/gopichandkomma/Desktop/certs/previewhealthchecker.edge.apple.com.chain.pem", "server key")
+	root = flag.String("root", "./certs/cacert.pem", "root CA")
+	cert = flag.String("cert", "./certs/server.pem", "server cert")
+	key  = flag.String("key", "./certs/server.key", "server key")
 )
 
 func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+
+	// Load the server certificate and its key
+	serverCert, err := tls.LoadX509KeyPair(*cert, *key)
+	if err != nil {
+		log.Fatalf("Failed to load server certificate and key. %s.", err)
+	}
+
+	// Load the CA certificate
+	trustedCert, err := os.ReadFile(*root)
+	if err != nil {
+		log.Fatalf("Failed to load trusted certificate. %s.", err)
+	}
+
+	// Put the CA certificate to certificate pool
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(trustedCert) {
+		log.Fatalf("Failed to append trusted certificate to certificate pool. %s.", err)
+	}
+
+	// Create the TLS configuration
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		RootCAs:      certPool,
+		ClientCAs:    certPool,
+		MinVersion:   tls.VersionTLS13,
+		MaxVersion:   tls.VersionTLS13,
+	}
+
+	// Create a new TLS credentials based on the TLS configuration
+	cred := credentials.NewTLS(tlsConfig)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	// options
 	var opts []grpc.ServerOption
-	if cert != nil && key != nil {
-		cert, err := tls.LoadX509KeyPair(*cert, *key)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		tlsConfig, err := applecerts.TLSConfig()
-		if err != nil {
-			log.Fatal(err)
-		}
-		tlsConfig.Certificates = []tls.Certificate{cert}
-		tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
-		opts = append(opts, grpc.Creds(credentials.NewTLS(tlsConfig)))
-	}
+	opts = append(opts, grpc.Creds(cred))
 
 	grpcServer := grpc.NewServer(opts...)
 	health.RegisterHealthServer(grpcServer, server.NewHealthServer())
