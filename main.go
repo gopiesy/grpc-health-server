464cadf1902d05cf62a0ab5249403b5de1fece42
GopiChand Komma <gkomma@apple.com>
2023-01-06 12:47:00 -0600

    remove vendor dir and include certs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"log"
@@ -8,11 +9,15 @@ import (
 
 	server "github.com/gopiesy/grpc-health-server/health-server"
 	health "github.com/gopiesy/grpc-health-server/proto"
+	"github.pie.apple.com/isd-dev/go-applecerts"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var (
 	port = flag.Int("port", 9111, "Port on which gRPC health server should listen for TCP conn.")
+	cert = flag.String("cert", "/Users/gopichandkomma/Desktop/certs/previewhealthchecker.edge.apple.com.chain.pem", "server cert")
+	key  = flag.String("key", "/Users/gopichandkomma/Desktop/certs/previewhealthchecker.edge.apple.com.chain.pem", "server key")
 )
 
 func main() {
@@ -21,7 +26,24 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	grpcServer := grpc.NewServer()
+	// options
+	var opts []grpc.ServerOption
+	if cert != nil && key != nil {
+		cert, err := tls.LoadX509KeyPair(*cert, *key)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tlsConfig, err := applecerts.TLSConfig()
+		if err != nil {
+			log.Fatal(err)
+		}
+		tlsConfig.Certificates = []tls.Certificate{cert}
+		tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+		opts = append(opts, grpc.Creds(credentials.NewTLS(tlsConfig)))
+	}
+
+	grpcServer := grpc.NewServer(opts...)
 	health.RegisterHealthServer(grpcServer, server.NewHealthServer())
 	log.Printf("Initializing gRPC health server on port %d", *port)
 	if err := grpcServer.Serve(lis); err != nil {
